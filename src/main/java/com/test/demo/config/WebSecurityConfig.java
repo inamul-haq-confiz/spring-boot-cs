ff9e2b930da1087a561ed2e4cdd8ac34857842bf
inamul-haq-confiz <inamul.haq@confiz.com>
2021-07-14 17:19:15 +0500

    unit tests added

--- src/main/java/com/test/demo/config/WebSecurityConfig.java
+++ src/main/java/com/test/demo/config/WebSecurityConfig.java
@@ -21,8 +21,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-				.authorizeRequests().antMatchers("/api/login",
-						"/api/register",
+				.authorizeRequests().antMatchers("/api/accounts/login",
+						"/api/accounts/register",
 						"/api/token",
 						"/swagger-ui.html",
 						"/swagger-resources/**",
